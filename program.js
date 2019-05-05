a4f61385957c60c782532337685e0fb61ce2d318
shiningnicholson95 <36324437+shiningnicholson95@users.noreply.github.com>
2019-05-05 11:06:22 +0530

    second commit with program

--- program.js
+++ program.js
@@ -1,2 +1,10 @@
-# TODO
-# Write a JavaScript program to check whether a number is prime of not
+// TODO
+// Write a JavaScript program to check whether a number is prime of not
+const num
+if(num%2!==0&&num%1==0)
+{
+    return true
+}
+else{
+   return false
+}
