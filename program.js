f34390421337b609eb517a5fc9c31f9a234af1e1
SATYAM <satyam.dev29@gmail.com>
2019-05-05 10:58:04 +0530

    Added my program for primary  number

--- program.js
+++ program.js
@@ -1,2 +1,13 @@
-# TODO
-# Write a JavaScript program to check whether a number is prime of not
+// TODO
+// Write a JavaScript program to check whether a number is prime of not
+
+function chkNumber(num){
+    if(num%2===0){
+        return ('It prime number' +num)
+    }
+    else{
+        console.log('its not prime number')
+    }
+}
+
+console.log(chkNumber(13))
